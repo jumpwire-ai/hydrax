ed9251b1d7e16c0f0ff36177bf2eae7b937cc9c4
William Huba <hexedpackets@gmail.com>
2021-11-17 15:04:53 -0500

    Pass through configuration of init options

--- lib/hydrax/application.ex
+++ lib/hydrax/application.ex
@@ -5,10 +5,14 @@ defmodule Hydrax.Application do
 
   @impl true
   def start(_type, _args) do
+    sup_opts = Application.get_env(:hydrax, :supervisor, [])
+    task_opts = Application.get_env(:hydrax, :task_supervisor, [])
+    reg_opts = Application.get_env(:hydrax, :registry, [])
+
     children = [
-      Hydrax.Registry,
-      Hydrax.Supervisor,
-      Hydrax.TaskSupervisor,
+      {Hydrax.Registry, reg_opts},
+      {Hydrax.Supervisor, sup_opts},
+      {Hydrax.TaskSupervisor, task_opts},
     ]
 
     opts = [strategy: :one_for_one, name: Hydrax.Application]
