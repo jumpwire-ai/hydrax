678a3fbb01189fe0adfa33adc7a974bc409aca5d
William Huba <hexedpackets@gmail.com>
2021-11-18 10:51:57 -0500

    Allow setting static children for the Supervisor via config

--- lib/hydrax/supervisor.ex
+++ lib/hydrax/supervisor.ex
@@ -4,6 +4,7 @@ defmodule Hydrax.Supervisor do
   """
 
   use Horde.DynamicSupervisor
+  require Logger
 
   def start_link(init_arg) do
     Horde.DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
@@ -13,11 +14,38 @@ defmodule Hydrax.Supervisor do
     Horde.DynamicSupervisor.start_child(__MODULE__, spec)
   end
 
+  def start_children(children) do
+    Logger.info("Starting #{inspect children}")
+    children
+    |> Stream.map(fn spec -> {spec, start_child(spec)} end)
+    |> Stream.filter(fn {_, result} ->
+      case result do
+        {:ok, _pid} -> false
+        {:error, {:already_started, _pid}} -> false
+        :ignore -> false
+        _ -> true
+      end
+    end)
+    |> Enum.each(fn res -> Logger.warn("Hydrax supervised child failed to start: #{inspect res}") end)
+  end
+
   @impl true
   def init(init_arg) do
-    [strategy: :one_for_one, members: :auto, process_redistribution: :active]
+    {children, init_arg} = Keyword.pop(init_arg, :children, [])
+    {delay, init_arg} = Keyword.pop(init_arg, :child_init_delay, 500)
+
+    init_arg = [strategy: :one_for_one, members: :auto, process_redistribution: :active]
     |> Keyword.merge(init_arg)
-    |> Horde.DynamicSupervisor.init()
+
+    case Horde.DynamicSupervisor.init(init_arg) do
+      {:ok, flags} ->
+        spawn(fn ->
+          :timer.sleep(delay)
+          start_children(children)
+        end)
+        {:ok, flags}
+      res -> res
+    end
   end
 
   def which_children(), do: Horde.DynamicSupervisor.which_children(__MODULE__)
